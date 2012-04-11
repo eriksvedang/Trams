b92deb516a1a63f9aa4da134568886cd827191d1
Erik Svedäng <erik.svedang@gmail.com>
2012-04-11 19:51:02 +0200

    Cleaned up the code a little.

--- Closet/src/Closet/core.clj
+++ Closet/src/Closet/core.clj
@@ -3,46 +3,44 @@
 	(:gen-class))
 
 (defn setup []
+	(no-stroke)
 	(frame-rate 30))
 	
-(defn wave [f min max rate]
-	"Generates a value that pulsates between min and max, using function f"
+(defn wave 
+	"Generates a value that pulsates between min and max at a set rate, using function f"
+	[f min max rate]
 	(let [height (- max min)
 		  half (/ height 2.0)
 		  mid (+ min half)
 		  t (/ (millis) 1000.0)]
-		(+ mid (* half (f (* t rate))))))
+		  (+ mid (* half (f (* t rate))))))
 		
-(defn square [x]
+(defn square 
 	"Generates a square wave"
+	[x]	
 	(if (pos? (sin x)) 1.0 -1.0))
 	
-(defn color-func [x y]
+(defn color-func 
 	"Returns an array with three values: [R, G, B]"
+	[x y]
 	(if (or (and (odd? x) (even? y)) (and (even? x) (odd? y)))
 		[(wave sin 100 255 3), (wave sin 100 255 4), (random 255)]
 		[(wave sin 100 255 5), (wave sin 100 255 6), (wave sin 100 255 7)]))
 
 (defn draw []
-	(no-stroke)
-	(let [step (wave sin 5 20 2.0) ;(inc (mouse-x))
+	(let [step (wave sin 5 20 2.0)
 		  x-steps (/ (width) step)
 		  y-steps (/ (height) step)
 		  cords (for [x (range x-steps) y (range y-steps)] [x y])]
 		  	(doseq [[x y] cords]
 				(apply fill (color-func x y))
 				(rect (* x step) (* y step) step step))))
-				
-(defn on-key []
-	(println "KEY:" (key-code))
-	(fill 255 0 0))
-	
+
 (defsketch example                
 	:title "SQUARES!"
-	:setup setup           
-	:draw draw  
-	:key-pressed on-key          
+	:setup setup
+	:draw draw         
 	:size [400 300])
-	
+
 (defn -main []
 	(println "Starting..."))
